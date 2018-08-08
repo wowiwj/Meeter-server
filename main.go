2e9cff5d5ebd7198feebf325240bd5773426bf1b
wowiwj <35649084@qq.com>
2018-08-08 19:14:53 +0800

    初始化配置信息

--- main.go
+++ main.go
@@ -4,10 +4,23 @@ import (
 	"Meeter/router"
 	orm "Meeter/database"
 	"github.com/spf13/pflag"
+	"Meeter/configs"
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
 
+var (
+	cfg = pflag.StringP("config", "c", "", "apiserver config file path.")
+)
+
 func main() {
+	pflag.Parse()
+
+	if err := configs.Init(*cfg);err != nil {
+		panic(err)
+	}
+
+	gin.SetMode(viper.GetString("runmode"))
 
 	router := router.Init()
 	defer orm.DB.Close()
