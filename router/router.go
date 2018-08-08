891e4b85005fd0f589e38d437c04b1c1141d05bd
wowiwj <35649084@qq.com>
2018-08-08 18:35:45 +0800

    添加env配置文件

--- router/router.go
+++ router/router.go
@@ -1,8 +1,18 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func Init() *gin.Engine  {
 	router := gin.Default()
+
+	router.GET("/ping", func(context *gin.Context) {
+		context.JSON(http.StatusOK,gin.H{
+			"message": "pong",
+		})
+	});
+
 	return router
 }
